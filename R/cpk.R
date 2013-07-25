667828946946865b94188302acdd86c53678905d
Oscar A. Linares, MD <OALinaresMD@gmail.com>
2013-07-25 00:00:00 +0000

    version 1.3

--- R/cpk.R
+++ R/cpk.R
@@ -1,8 +1,9 @@
 # Oscar A. Linares MD and David T. Daly JD/MBA
 # Plymouth Pharmacokinetic Modeling Study Group
+# Plymouth, Michigan 48170
 #
-# cpk Version 1.2
-# 7/20/2013
+# cpk Version 1.3
+# 7/24/2013
 
 #####################################
 # R FUNCTIONS
@@ -14,7 +15,7 @@ ttc.fn <- function(msc, mec) {
   denominator <- log(msc/mec)
   result      <- numerator/denominator
   print(paste("The value of ttc (ug/L) is", round(result,2), sep=" "))
-  return(round(result,0))
+  return(round(result,2))
 }
 
 # dose rate
@@ -22,9 +23,9 @@ dr.fn <- function(ttc, cl, wtkg, f) {
   
   numerator   <- ttc * cl * wtkg
   denominator <- f
-  result      <- numerator/denominator * 0.001
-  print(paste("The value of dr (mg/h) is", round(result,2), sep=" "))
-  return(round(result,2))
+  result      <- (numerator/denominator) * 0.001
+  print(paste("The value of dr (mg/h) is", round(result,3), sep=" "))
+  return(round(result,3))
   
 }
 
@@ -40,18 +41,18 @@ di.fn <- function (msc, mec, ke) {
 
 # maintenance dose
 dm.fn <- function (dr, di) {
-  result <- dr * di
+  result <- dr * di * 1000
   print(paste("The value of dm (mg every di h) is", round(result,2), sep=" "))
   return(round(result,2))
 }
 
 # back calculation TTC
-bc.ttc.fn <- function (dr, f, cl, wtkg = 86) {
+bc.ttc.fn <- function (dr, f, cl, wtkg) {
   numerator   <- dr * 1000 * f
   denominator <- cl * wtkg
   result      <- numerator/denominator
   print(paste("The value of bc.ttc (ug/L) is", round(result,1), sep=" "))
-  return(round(result,1))
+  return(round(result,2))
 }
 
 # accumulation rate
@@ -65,13 +66,13 @@ ar.fn <- function(ke, di) {
 
 # Oral dose
 dpo.fn <- function(dr, di) {
-  result      <- dr * di * 1000
-  print(paste("The value of dpo (ug) is", round(result,2), sep=" "))
-  return(round(result,2))
+  result      <- dr * di
+  print(paste("The value of dpo (ug) is", round(result,3), sep=" "))
+  return(round(result, 3))
 }
 
 # Cmax
-cmax.fn <- function(f, dpo, vd, ar, wtkg = 86) {
+cmax.fn <- function(f, dpo, vd, ar, wtkg) {
   numer <- f * dpo
   denom <- vd * wtkg
   result      <- (numer/denom) * ar
@@ -87,11 +88,41 @@ cmin.fn <- function(cmax, ke, di) {
 }
 
 # Css
-css.fn <- function(f, dpo, di, cl, wtkg = 86) {
+css.fn <- function(f, dpo, di, cl, wtkg) {
   numer  <- f * (dpo/di)
   denom  <- cl * wtkg
-  result <- (numer/denom)  
+  result <- (numer/denom) 
   print(paste("The value of css (ug/L) is", round(result,2), sep=" "))
   return(round(result,2))
+}
+
+# Ct - clearance curve (concentration time curve)
+ct.fn <- function (cmax, ke, time=0) {
+        result <- cmax * exp(-ke * time)
+        print(paste("The value of ct (ug/L) at t h is", round(result, 2), sep = " "))
+        return(round(result, 2))
+}
+
+# loading dose cmax
+dlcmax.fn <- function(cmax,vd){
+              result <- cmax * vd
+              print(paste("The value of dlcmax (mg) is", round(result, 2), 
+              sep = " "))
+              return(round(result, 2))
+}
+
+# loading dose ar
+dlar.fn <- function(dm, ar){
+            result <- dm * ar
+            print(paste("The value of dlar (mg) is", round(result, 2), 
+              sep = " "))
+            return(round(result, 2))
+}
+
+# infusion rate R0 (mg/h)
+R0.fn <- function(css, cl) {
+           result <- css * cl
+           print(paste("The value of R0 (ug/hr) is", round(result, 2), sep = " "))
+           return(round(result, 2))
 }
 ####################### END ####################### 
